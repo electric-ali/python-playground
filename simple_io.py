01b0a3dd39835fbe51d774edadff69701e10d30c
ar4f4t <43527812+ar4f4t@users.noreply.github.com>
2021-06-01 12:10:17 -0400

    Add files via upload

---
+++ simple_io.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+# Simply prints a string output which is given by the user as an input.
+
+def take_input():
+	x = input("Type your input here: ")
+	return x
+
+print("Your entered Text: " + take_input())
